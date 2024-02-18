de58aa7d4d9cfc57abc968901b671f4a7c52c31c
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-02-18 12:18:06 +0530

    Add files via upload

---
+++ add saved.py
@@ -0,0 +1,9 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: E:/Vs Code/Python/add.py
+Hi
+ I am Mourya
+Enterfirst number=33
+Enter second number=36
+69
