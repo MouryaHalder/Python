3de4e68a9b23451e968e88f87a9bd1b570805601
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-02-19 11:18:28 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,28 @@
+#  a=1 (int data type)
+#  b=true (bool)
+#  c=harry (string)
+#  d= none (none type)
+a=1.2
+d=complex(8,2)
+b= True
+c="Sumit"
+a1=9
+print(a)
+print(b)
+print(c)
+print(a+a1)
+print("The type of a is", type(a))
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+print("The type of d is", type(d))
+# List is collection of data elements (list of items)
+# tuple  same just immutable(unchangable)
+# mapped data type  (collection of key value pairs) dictionary
+list=[8,2.3,[-4,5],["apple","banana"]]
+print (list)
+
+tuple=(("Parrot","Sparrow"),("Lion","Tiger"))
+print (tuple)
+
+dict = {"Name":"Mourya","age":19,"CanVote":True}
+print(dict)
