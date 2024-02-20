29872deb6328d4ad214b95982f0035861de58ff8
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-02-20 12:39:51 +0530

    Add files via upload

---
+++ DocString.py
@@ -0,0 +1,11 @@
+# doc string and pep - 8
+# Python docstrings are the string literals that appear right after the definition of a function,method,class,or module
+def square(n):
+    '''Takes in a number n,returns the squareof n'''
+    print(n**2)
+square(5)
+print(square.__doc__) #(DOC STRING)this will print the string
+#Doc string must be just under def function body
+
+#PEP-8
+# Python Enhancement Proposal (PEP)
