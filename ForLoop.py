b50646add63c6908299f93131fb2b3f781ad10ea
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-02-21 13:24:22 +0530

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,22 @@
+# name= 'Mourya'
+# for  i in name:
+#     print(i,end = ", ") # if i dont use end the nxt letter will go next line
+#     if(i == "a"):
+#      print("This is something special")
+
+
+colors = ["Red","Green","Blue","Black","Pink"]
+for color in colors:
+    print(color) 
+    for i in color:
+        print(i)
+
+# Range function
+for k in range (5): # aotomatically 0 to 5 print hobe
+    print(k+1)
+
+for k in range (1,9):
+    print(k)
+
+for k in range (1,50):
+    print(k)
