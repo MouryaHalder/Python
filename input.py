ff692d01724c01d935084f826d2d961e964d7195
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-02-25 12:19:59 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,11 @@
+a= input("Enter your name:")
+print("My name is",a)
+
+c=input("Enter a number")#Inputting as a STRING
+b=input("Enter a number")
+print(c+b) # String is concatenation / combining 
+print(int(c)+int (b)) #changing DATA TYPE 
+
+e=5.2
+f=7.8
+print(e+f)              
