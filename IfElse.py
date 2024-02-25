15de210b2a218f99375219533f7e5cd91b1ecdb9
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-02-25 12:19:45 +0530

    Add files via upload

---
+++ IfElse.py
@@ -0,0 +1,17 @@
+a= int(input("Enter your age:"))
+print("Your age is:",a)
+
+if(a>18):
+    print("You can Drive")
+else:
+    print("You can not drive")
+
+applePrice=10
+budget=20
+if(budget-applePrice > 5):
+  print("Alexa, add 1kg apple to the cart.")
+elif(budget-applePrice>7):
+   print("Its okay you can buy")
+else:
+  print("Alexa, do not add apple to the cart")
+
