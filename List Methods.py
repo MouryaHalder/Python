70a8e883954cede09ee60e0945eeba9da2703159
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-02-25 12:20:13 +0530

    Add files via upload

---
+++ List Methods.py
@@ -0,0 +1,20 @@
+l=[81,72,69,99,110,1,2,3,4,5,6]
+# print(l)
+# l.append(7) #append means adding 7
+# print(l)
+# l.sort()  #Sorting from ascending to decending order
+# print(l)
+# l.sort(reverse=True) # thos method reverse the list
+# print(l)
+# print(l.index(2))
+# print(l)
+# print(l.count(1)) # counting how many times 1 came
+# print(l)
+# m=l.copy()  # making list copy
+# m[0]=0
+#l.insert(1,899) #899 index 1 a insert hobe
+m=[900,1000,1100]
+k=l+m # 2 list elements will merge 
+print(k)
+#l.extend(m) # l list ta khule m ar value m ar last a dhokao
+print(l)
