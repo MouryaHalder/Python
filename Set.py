77be48397afad826ac21ffd1a428cab20ef08d56
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-03-02 21:34:12 +0530

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,10 @@
+s={2,5,2,9,6}
+print(s)
+
+info = {"Carla",18,False,5.5,19} #it does not maintain order in python
+print(info) 
+
+mourya={} #this is empty dictionarry
+print(type(mourya)) 
+mourya=set() #this is empty set
+print(type(mourya)) 
