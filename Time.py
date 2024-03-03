132c4f12704a0d11f51d1a629459eeed60cf3ea5
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-03-04 03:20:03 +0530

    Add files via upload

---
+++ Time.py
@@ -0,0 +1,16 @@
+import time
+timestamp =time.strftime('%H:%M:%S:%p') #strftime is a function which will give hours and mins
+print (timestamp)
+timestamp=time.strftime('%H')
+print(timestamp)
+timestamp=time.strftime('%M')
+print(timestamp)
+timestamp=time.strftime('%S')
+print(timestamp)
+
+timestamp=time.strftime('%p')
+print(timestamp)
+
+timestamp=time.strftime('%B')
+print(timestamp)
+
