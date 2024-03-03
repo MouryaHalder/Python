2bce015abeeaf7cf14e16f7b86ca28c239acc5ec
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-03-03 13:05:54 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,16 @@
+name="Sumit"
+Friend="Rohan"
+print(name,Friend)
+apple='He said,"I want to eat apple" '
+print(apple)
+print(name[0])#Indexing
+print(name[1])#Indexing
+print(name[2])
+print(name[3])
+print(name[4])
+#print(name[5]) this throws an error
+
+# For loop in strings
+print("Let's use a FOR LOOP")
+for character in apple:
+    print(character)
