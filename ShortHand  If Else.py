0c5db967f4ec8e616a9b918e9ec2d3e324516d7e
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-04-03 17:40:53 +0530

    Add files via upload

---
+++ ShortHand  If Else.py
@@ -0,0 +1,10 @@
+a=350
+b=330
+print('A') if a>b else print('=')  if a == b else print('B')
+#Conditional expression
+c= 9 if a>b  else 0
+print(c)
+
+d=350
+e=350
+print('A') if d>e else print('=')  if d == e else print('B')
