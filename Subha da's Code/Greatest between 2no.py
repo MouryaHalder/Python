fe0bafa6ba9336517e71cb8f84f33f32ec298296
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-05-11 14:25:44 +0530

    Add files via upload

---
+++ Subha da's Code/Greatest between 2no.py
@@ -0,0 +1,8 @@
+# Write a Python Program (WAPP) to check greater between two numbers ( A!=B)
+a=int(input("Enter 1st number="))
+b=int(input("Enter 2nd number="))
+if(a>b):
+    big=a
+else:
+    big=b
+print(big,"is Largest number")
---
+++ Subha da's Code/Nested1.py
@@ -0,0 +1,7 @@
+n=0
+if(n>0):
+    print(n,"is positive number")
+elif(n==0):
+    print(n,"is equvalent to zero")
+else:
+    print(n,"is negative number")
