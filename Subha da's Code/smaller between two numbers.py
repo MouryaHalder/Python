f1fba61d27c51707d6ccce24486c1607f80fd42e
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-05-12 14:01:56 +0530

    Add files via upload

---
+++ Subha da's Code/smaller between two numbers.py
@@ -0,0 +1,8 @@
+# Write a Python Program (WAPP) to check smaller between two numbers ( A!=B)
+a=int(input("Enter 1st number="))
+b=int(input("Enter 2nd number="))
+if(a<b):
+    small=a
+else:
+    small=b
+print(small,"is smallest number")
