9c946d790a1e8f77a120d84339a0cced3797cdd9
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2024-05-13 19:00:40 +0530

    Add files via upload

---
+++ Subha da's Code/check positive or negative.py
@@ -0,0 +1,8 @@
+# WAPP to check positive or negative.
+n=int(input("Enter any number="))
+if(n>0):
+    print(n,"is positive number")
+elif(n==0):
+    print(n,"is equvalent to zero")
+else:
+    print(n,"is negative number")       
